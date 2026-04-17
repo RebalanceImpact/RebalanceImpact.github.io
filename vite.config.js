e7c069aa11407667d801b18fe71380833a377448
SteveMaycock <steviefromwindband@gmail.com>
2026-04-17 10:46:57 +0200

    sitemap

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,35 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
+import Sitemap from 'vite-plugin-sitemap';
+import { articles } from './src/data/articleContent.js';
+
+
+// 1. Map through your data file to get all the article slugs
+const articleRoutes = articles.map(article => `/insights/${article.slug}`);
+
+const coreRoutes = [
+  '/',
+  '/about-us',
+  '/esg-services',
+  '/sustainable-reporting',
+  '/data-kpis',
+  '/news-media',
+  '/new-to-esg-reporting'
+];
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
+  plugins: [
+    react(), 
+    tailwindcss(),
+    Sitemap({
+      hostname: 'https://www.rebalanceimpact.com',
+      dynamicRoutes: [...coreRoutes, ...articleRoutes], // Combine core routes with article routes
+      outDir: 'dist',
+      generateRobotsTxt: true,
+    }),
+  ],
   build: {
     outDir: 'dist',
     sourcemap: false,
